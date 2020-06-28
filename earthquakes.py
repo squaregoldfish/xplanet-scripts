561c84984a0b34722b39e28a5958104c8a7a8f83
Steve Jones <steve@squaregoldfish.co.uk>
2020-06-28 13:27:40 +0200

    Documentation

--- earthquakes.py
+++ earthquakes.py
@@ -3,24 +3,42 @@ import sys
 import urllib.request
 import json
 
+# Get latest earthquake data from USGS and generate an Xplnet marker file
+#
+# Usage: earthquakes.py output_file min_strength
+#
+# output_file  is the name of the marker file to generate
+# min_strength will filter the earthquakes so that quakes with a magnitude
+#              less than this value will not be included
+
+# The URL for the USGS JSON - all quakes for the last 7 days
 URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
+# The main functio
 def main(output_file, min_strength):
+
+  # Download the JSON from USGS
   with urllib.request.urlopen(URL) as url:
     data_string = url.read().decode()
 
-  # For debugging
+  # For debugging - download the USGS json into a file
   #with open("quake.json", "r") as f:
   #  data_string = f.read()
 
-  # The USGS JSON comes with single quotes which is invalid
+  # Parse the JSON
   data = json.loads(data_string)
+
+  # Open the output file
   with open(output_file, 'w') as f:
 
+    # Loop through each quake
     for quake in data["features"]:
-      mag = quake["properties"]["mag"]
 
+      # Check the magnitude
+      mag = quake["properties"]["mag"]
       if mag >= min_strength:
+
+        # Extract details and write line to the output file
         lon = quake["geometry"]["coordinates"][0]
         lat = quake["geometry"]["coordinates"][1]
         alert = quake["properties"]["alert"]
